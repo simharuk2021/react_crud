91c2d3956fb90091956a62ee641648251de6eb05
Simharuk2021 <simharuk@yahoo.co.uk>
2023-05-19 14:19:18 +0100

    styling changes made

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function Crud() {
   const [newItem, setNewItem] = useState("");
   const [editingItemId, setEditingItemId] = useState(null);
   const [editingItemValue, setEditingItemValue] = useState("");
+  const [error, setError] = useState("");
 
   useEffect(() => {
     getItems();
@@ -13,6 +14,13 @@ function Crud() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (newItem.trim() === "") {
+      setError("Please enter an item name");
+      return;
+    }
+    setError("");
+
     database.ref("items").push({ name: newItem });
     setNewItem("");
   };
@@ -40,11 +48,13 @@ function Crud() {
 
   const handleDelete = (id) => {
     database.ref(`items/${id}`).remove();
+    setError(""); // Clear error message
   };
 
   const handleEdit = (id, name) => {
     setEditingItemId(id);
     setEditingItemValue(name);
+    setError(""); // Clear error message
   };
 
   const handleCancelEdit = () => {
@@ -57,43 +67,52 @@ function Crud() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={newItem}
-          onChange={(e) => setNewItem(e.target.value)}
-          placeholder="Add a new item"
-        />
-        <button class = "add" type="submit">Add</button>
-      </form>
-      <ul>
-        {items.map((item) => (
-          <li key={item.id}>
-            {editingItemId === item.id ? (
-              <>
-                <input
-                  type="text"
-                  value={editingItemValue}
-                  onChange={handleInputChange}
-                />
-                <button onClick={() => handleUpdate(item.id, editingItemValue)}>
-                  Save
-                </button>
-                <button onClick={handleCancelEdit}>Cancel</button>
-              </>
-            ) : (
-              <>
-                {item.name}
-                <button class = "edit" onClick={() => handleEdit(item.id, item.name)}>
-                  Edit
-                </button>
-                <button class = "delete" onClick={() => handleDelete(item.id)}>Delete</button>
-              </>
-            )}
-          </li>
-        ))}
-      </ul>
+    <div className="container">
+      <div className="content">
+       <h2 className="title">REACT and FIREBASE TO DO APP</h2>
+        <div class="input-container">
+          {error && <p className="error">{error}</p>}
+          <form
+            class="add-form"
+            onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={newItem}
+              onChange={(e) => setNewItem(e.target.value)}
+              placeholder="Add a new item"
+            />
+            <button class="add" type="submit">Add</button>
+          </form>
+          <ul>
+            {items.map((item) => (
+              <li key={item.id}>
+                {editingItemId === item.id ? (
+                  <>
+                    <input
+                      class="edit-form"
+                      type="text"
+                      value={editingItemValue}
+                      onChange={handleInputChange}
+                    />
+                    <button onClick={() => handleUpdate(item.id, editingItemValue)}>
+                      Save
+                    </button>
+                    <button onClick={handleCancelEdit}>Cancel</button>
+                  </>
+                ) : (
+                  <>
+                    {item.name}
+                    <button class="edit" onClick={() => handleEdit(item.id, item.name)}>
+                      Edit
+                    </button>
+                    <button class="delete" onClick={() => handleDelete(item.id)}>Delete</button>
+                  </>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
